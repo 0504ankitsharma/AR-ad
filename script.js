78edf9b8db313ca700f403e83b66e0fbfc97419d
0504ankitsharma <0504ankitsharma@gmail.com>
2025-02-04 06:40:53 +0530

    first commi32t

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-function toggleVideo() {
+// Check for browser compatibility
+if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    document.getElementById("compatibility-warning").style.display = "block";
+  }
+  
+  // Toggle video play/pause
+  function toggleVideo() {
     const video = document.getElementById("ar-video");
     if (video.getAttribute("autoplay")) {
       video.removeAttribute("autoplay");
       video.pause();
+      document.querySelector("a-text").setAttribute("value", "Play");
     } else {
       video.setAttribute("autoplay", true);
       video.play();
+      document.querySelector("a-text").setAttribute("value", "Pause");
     }
   }
